c1b6c9dd3148f4978f87f722e4474759a7e55844
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-10-16 22:50:50 +0530

    Fix CheckFileLinked

--- 04.File Mangement/ChechFileisLinked.c
+++ 04.File Mangement/ChechFileisLinked.c
@@ -1,10 +1,20 @@
 //write a program to know is the given files are linked or not b/w two files and what type of link
+//./ChechFileisLinked HardLink/Original.txt HardLink/HardLink.txt
+//./ChechFileisLinked SoftLink/Original.txt SoftLink/SoftLink.txt
+//./ChechFileisLinked NoLink/Original.txt NoLink/data.txt
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
 
 int main(int argc,char*argv[]){
+
+    if (argc != 3) {
+        printf("Usage: %s <file1> <file2>\n", argv[0]);
+        return 1;
+    }
+
     struct stat data1;
     struct stat data2;
 
@@ -13,12 +23,31 @@ int main(int argc,char*argv[]){
 
     if(data1.st_ino != data2.st_ino){
         printf("No Link is Made Between %s and %s\n",argv[1],argv[2]);
+        return 0;
     }
 
-    if(data1.st_blocks == data2.st_blocks){
+    struct stat data1_ls;
+    struct stat data2_ls;
+
+    lstat(argv[1],&data1_ls);
+    lstat(argv[2],&data2_ls);
+
+    if(data1_ls.st_ino == data2_ls.st_ino ){
+        printf("\nHardlink: Based on Inode Size\n");
+    }
+    else if(data1_ls.st_ino != data2_ls.st_ino){
+        printf("SoftLink: Based on Inode Size\n");
+    }
+    
+    printf("Inode NUmber: %lu %lu\n\n",data1_ls.st_ino,data2_ls.st_ino);
+
+
+    if(data1_ls.st_blocks == data2_ls.st_blocks){
         printf("Hardlink: Based on Block Size\n");
-    }else{
+    }else if(data1_ls.st_blocks != data2_ls.st_blocks){
         printf("SoftLink: Based on Block Size\n");
     }
+
+    printf("Block Size: %lu %lu\n",data1_ls.st_blocks,data2_ls.st_blocks);
 
 }
